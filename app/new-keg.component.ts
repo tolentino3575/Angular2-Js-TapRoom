ad9f565c3a9ae770b6b44f8655b33353072ec7cd
erik tolentino and yvonna contreras <tolentino3575@gmail.com>
2016-03-23 15:39:22 -0700

    Create new-keg component file

---
+++ app/new-keg.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'new-keg',
+  template: `
+    <h4>Blank</h4>
+  `
+})
+
+export class NewKegComponent{
+  
+}
