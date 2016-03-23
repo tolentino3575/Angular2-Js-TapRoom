5a1012a23e26a9044ead5853fc84804173bef599
erik tolentino and yvonna contreras <tolentino3575@gmail.com>
2016-03-23 10:09:39 -0700

    Create new instances of keg in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,18 +3,30 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="page-header">
+    <h1>Beer!</h1>
+  </div>
+  <div *ngFor="#keg of kegs" class="container">
+    <h3 >{{ keg.name }}</h3>
+    <h4>Drink Me</h4>
     </div>
   `
 })
 
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor(){
+    this.kegs = [
+      new Keg("RPM IPA", "Boneyard", 100, 7, 124),
+      new Keg("Sculpin", "Ballast Point", 125, 7, 124),
+      new Keg("Hef", "Widmer", 100, 6, 124),
+      new Keg("Arrogant Bastard", "Stone", 130, 8, 124)
+    ];
+  }
 }
 
 export class Keg {
-  constructor(public name: string, public brand: string, public price: number, public alchohol: number){
-    
+  constructor(public name: string, public brand: string, public price: number, public alcohol: number, public pints: number){
+
   }
 }
